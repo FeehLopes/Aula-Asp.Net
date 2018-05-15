6b93357a38f88b4b2d327415f0d6ebcb84ee47e3
FELIPE <FELIPE@FELIPE-PC>
2018-05-15 20:15:03 -0300

    ADICIONANDO CATEGORIA/INDEX

---
+++ Aula_1505_MVC1/Aula_1505_MVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aula_1505_MVC1.MvcApplication" Language="C#" %>
---
+++ AulasNet/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿RESTORE FILELISTONLY
+
+FROM DISK = 'C:\Users\FELIPE\Downloads\AdventureWorks2016.bak'
+
+GO
+
+RESTORE DATABASE ADWORKSDB
+
+FROM DISK = 'C:\Users\FELIPE\Downloads\AdventureWorks2016.bak'
+WITH MOVE 'AdventureWorks2016_Data' TO 'C:\Users\FELIPE\Downloads\AdventureWorks2016.MDF',
+MOVE 'AdventureWorks2016_Log ' TO 'C:\Users\FELIPE\Downloads\AdventureWorks2016.LDF'
